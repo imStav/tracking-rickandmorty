5d78a8d420fc7e2735b2bab75e37adf57c9d9a37
imStav <saprocontact@gmail.com>
2022-07-26 12:15:29 +0200

    New UI feature
    Added motion transitions.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { MotionPlugin } from "@vueuse/motion";
 import "./index.css";
 
 import App from "./App.vue";
@@ -9,5 +10,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(MotionPlugin);
 
 app.mount("#app");
