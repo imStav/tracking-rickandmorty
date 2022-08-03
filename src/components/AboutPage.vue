876b4046715153e42300362ca53e546bb292db32
imStav <saprocontact@gmail.com>
2022-08-03 16:08:20 +0200

    Updating About section

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -12,7 +12,7 @@ const title = ref("About");
       still alive in the current TV show's timeline.
     </p>
 
-    <p class="my-8">
+    <p class="my-4">
       All the data are retrieved thanks to the Rick & Morty API at
       <a
         href="https://rickandmortyapi.com/"
@@ -23,7 +23,11 @@ const title = ref("About");
       >
     </p>
 
-    <p>
+    <p class="my-4">
+      Rick and Morty is created by Justin Roiland and Dan Harmon for Adult Swim. The data and images are used without claim of ownership and belong to their respective owners.
+    </p>
+
+    <p class="my-4">
       This project is subject to changes / evolutions in the future. I'm working
       on new features, such as an integrated search engine, filters, etc.
     </p>
